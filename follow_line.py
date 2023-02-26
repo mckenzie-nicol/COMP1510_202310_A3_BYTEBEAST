eab2b9f59dde80435b7728eefeb2e899e61244db
Mckenzie Nicol <mnicol11@my.bcit.ca>
2023-02-26 13:47:58 -0800

    Simplify algorithm for follow_line.py
    Remove sum_of_error and related variables
    Remove anticipate_error and related variables

--- follow_line.py
+++ follow_line.py
@@ -11,47 +11,26 @@ motor_pair = MotorPair('A', 'B')
 motor_pair.set_default_speed(50)
 colour = ColorSensor('E')
 # When bot sensor is placed on 50% black and 50% other colour, the amount of reflected light becomes our target.
-# The highter the steer_factor, the more drastic corrections the bot will make. Lower steer_factor, the smoother the
-# corrections will be.
+# The highter the steer_factor, the more drastic corrections the bot will make. Lower steer_factor, the smoother the corrections will be.
 
 target = colour.get_reflected_light()
 steer_factor = 1
 
-# When initially determining the steer_factor, set error_factor and anticipate_factor to 0. Once the steer_factor is
-# determined, adjust error_factor and anticipate_factor.
-# Overall, the sum_of_error should be relatively close to zero, as the same errors are expected to happen on either
-# side. Negative errors to the left, and positive errors to the right.
-
 error = 0
-sum_of_error = 0
-error_factor = 0
-
-anticipate_error = 0
-last_error = 0
-anticipate_factor = 0
 
 # Write your program here.
 hub.speaker.beep()
 # Ask the distance sensor to take a reading.
 distance_sensor.get_distance_cm()
-# If no obstacle is detected within 10cm, start the motor and drive in a straight direction. Calculate the light
-# difference (% of black tape vs % of other background)
+# If no obstacle is detected within 10cm, start the motor and drive in a straight direction. Calculate the light difference (% of black tape vs % of other background)
 while distance_sensor > 10 or None:
     motor_pair.start()
     light_difference = target - colour.get_reflected_light()
-# If the light_difference varies by more than +/- 2, the bot will evaluate the error by subtracting the light_difference
-    # from the initial target value.
-    while light_difference < -2   or light_difference > 2:
+    # If the light_differnce varies by more than +/- 2, the bot will evaluate the error by subtracting the light_difference from the inital target value.
+    while light_difference < -2 or light_difference > 2:
         error = target - light_difference
-        sum_of_error += error
-        anticipate_error = error - last_error
-# The steer_amount is calculated by adding 3 factors. The error, the ongoing sum of errors, and the anticipated error
-        # based on the most recent error.
-        steer_amount = (error * steer_factor) + (sum_of_error * error_factor) + (anticipate_error * anticipate_factor)
-# The current error becomes the last error, and then the bot is steered based on this calculation.
-        last_error = 0
-        last_error += error
+        # The steer_amount is calculated the error multiplied by the steering factor.
+        steer_amount = error * steer_factor
         motor_pair.start(steer_amount)
-# The distance sensor is run at the end of the loop and will stop the bot if it encounters an obstacle. If no obstacle
-    # is detected, the loop is iterated.
+    # The distance sensor is run at the end of the loop and will stop the bot if it encounters an obstacle. If no obstacle is detected, the loop is iterated.
     distance_sensor.get_distance_cm()
